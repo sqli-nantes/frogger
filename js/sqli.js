c5d8a6fb461d481ac6d1985fe0948c6cbbf4f69e
jefBinomed <jean.francois.garreau@gmail.com>
2015-05-21 23:18:00 +0200

    callibrage du saut

--- js/sqli.js
+++ js/sqli.js
@@ -2,9 +2,10 @@ var FROGGER_SQLI = FROGGER_SQLI || function(){
 	
 	var socket = null,
 		timeStep = 0,
-		trackMovement = false,
-		backMovement = false,
-		sendEvent = false;
+		step1 = false,
+		step2 = false,
+		step3 = false,
+		lastMax = 0;
 
 	function init(){		
 		socket = io();
@@ -35,6 +36,12 @@ var FROGGER_SQLI = FROGGER_SQLI || function(){
 			
 		});
 
+		var resetElemnt = document.querySelector('.reset');
+		resetElemnt.addEventListener('click', function(){
+			game.lives = 5;
+			game.reset();
+		});
+
 		socket.on('message', function(message){
 			if (message.action && message.action === 'jump'){
 				up();
@@ -44,47 +51,58 @@ var FROGGER_SQLI = FROGGER_SQLI || function(){
 
 	function initPhone(){
 		window.addEventListener('devicemotion', function(motion){
-			/*console.info("Acc : %d/%d/%d | Acc Gravity %d/%d/%d ",
-				motion.acceleration.x, 
-				motion.acceleration.y, 
-				motion.acceleration.z, 
-				motion.accelerationIncludingGravity.x,
-				motion.accelerationIncludingGravity.y,
-				motion.accelerationIncludingGravity.z
-				);*/
-			var data = motion.acceleration.y;
+			
+			/* 
+			Un saut suit une courbe d'acceleration de Y comme suit : 
+				Etape 1. : l'accélération dépasse 10 en valeur absolue (on est dans la montée)
+				Etape 2. : L'accélération s'arrete 
+				Etape 3 : L'accélération va dépasser 10 en valeur absolue (on est dans la descente)
+				Etape 4 : l'accélération s'arrete (on a finit notre mouvement)
+			*/
+
+
+
+			var data = Math.abs(motion.acceleration.y);
 			//console.warn("Acc : %s",data);
-			if (data > 5){
-				if (!trackMovement){					
-					trackMovement = true;
-					console.info("Track Movement Acc : %s ",data);					
-					timeStep = new Date().getTime();
-				}else if (sendEvent && !backMovement){
-					console.info("Back Movement Acc : %s ",data);					
-					backMovement = true;
-					timeStep = new Date().getTime();
-				}
-			}else if (trackMovement && !sendEvent && data < 0){
-				sendEvent = true;
-				console.info("Send jump instruction ");					
-				socket.emit('message',{action: 'jump'});
+			if (data > 10){				
 				timeStep = new Date().getTime();
-			}else if (trackMovement && sendEvent && backMovement && data < 0){
-				console.info("Reset ");				
-				trackMovement = false;
-				backMovement = false;
-				sendEvent = false;
-				timeStep = new Date().getTime();	
+				if (lastMax < data){
+					lastMax = data;
+				}else if (!step1){
+					step1 =  true;
+					socket.emit('message',{
+						action : "jump"
+					});
+					console.info('Jump');
+				}else if (step2 && !step3){
+					step3 = true;
+				}
+			}else if (step1){
+				lastMax = 0;
+				if (!step2){
+					step2 = true;
+				}else if (step3){					
+					//console.info('Reset');
+					step1 = false;
+					step2 = false;
+					step3 = false;					
+				}
 			}
+
+
+			
 		}, true);
 
 		setInterval(function(){
-			if (trackMovement){
+			if (step1){
 				var timestamp = new Date().getTime();
 				if (timestamp - timeStep > 1000){
-					backMovement = false;
-					trackMovement = false;
-					sendEvent = false;
+					console.info("Clear ");			
+					// S'il ne s'est rien passé depuis 1 seconde, alors on réinitialise tout
+					step1 = false;
+					step2 = false;
+					step3 = false;
+					lastMax = 0;
 				}
 			}
 		},1000);
