c3a4eb239d06e1788380c5b36fb13194283c3d66
jefBinomed <jean.francois.garreau@gmail.com>
2015-05-27 14:11:56 +0200

    mise en place des deplacements lateraux

--- js/sqli.js
+++ js/sqli.js
@@ -6,7 +6,8 @@ var FROGGER_SQLI = FROGGER_SQLI || function(){
 		step2 = false,
 		step3 = false,
 		lastMax = 0,
-		boing = false;
+		boing = false,
+		touch = {};
 
 	function init(){		
 		socket = io();
@@ -14,7 +15,7 @@ var FROGGER_SQLI = FROGGER_SQLI || function(){
 		    console.log(data);
 		});		
 
-		checkConfiguration();		
+		window.addEventListener('load', checkConfiguration);
 	}
 
 	function checkConfiguration(){
@@ -55,58 +56,91 @@ var FROGGER_SQLI = FROGGER_SQLI || function(){
 				boing.play();
 				boing.loop = false;
 				up();
+			}else if (message.action && message.action === 'left'){
+				left();
+			}else if (message.action && message.action === 'right'){
+				right();
 			}
 		});
 	}
 
+	function motionCallBack(motion){
+		/* 
+		Un saut suit une courbe d'acceleration de Y comme suit : 
+			Etape 1. : l'accélération dépasse 10 en valeur absolue (on est dans la montée)
+			Etape 2. : L'accélération s'arrete 
+			Etape 3 : L'accélération va dépasser 10 en valeur absolue (on est dans la descente)
+			Etape 4 : l'accélération s'arrete (on a finit notre mouvement)
+		*/
+
+		var data = Math.abs(motion.acceleration.y);
+		//console.warn("Acc : %s",data);
+		if (data > 10){				
+			timeStep = new Date().getTime();
+			if (lastMax < data){
+				lastMax = data;
+			}else if (!step1){
+				step1 =  true;
+				socket.emit('message',{
+					action : "jump"
+				});
+				console.info('Jump');
+			}else if (step2 && !step3){
+				step3 = true;
+			}
+		}else if (step1){
+			lastMax = 0;
+			if (!step2){
+				step2 = true;
+			}else if (step3){					
+				//console.info('Reset');
+				step1 = false;
+				step2 = false;
+				step3 = false;					
+			}
+		}
+	}
+
+	function touchCallBack(event){
+		event.preventDefault();
+		if (event.type === 'touchstart'){
+			touch = event.changedTouches[0];
+		}else if (event.type === 'touchend'){
+			var touchTmp = event.changedTouches[0];
+
+				// On doit inverser les données et on ne s'occuper que du X
+				/* 
+				  Dans ce schema le smartphone nous fait fasse
+				 y
+				 ^      -----
+				 |     |	 |		
+				 |     |     |
+				 |     |     |
+				 |      -----
+				 0------------> x
+				 
+				Xstart - Xend < 0 
+					=> On glisse le doigt vers la droite 
+					=> On veut aller à gauche dans le jeux
+
+				*/
+			if (touch.clientX - touchTmp.clientX < 0){
+				socket.emit('message',{
+					action : "left"
+				});
+			}else{
+				socket.emit('message',{
+					action : "right"
+				});
+			}
+		}
+		console.log(event);
+	}
+
 	function initPhone(){
-		window.addEventListener('devicemotion', function(motion){
-			
-			/* 
-			Un saut suit une courbe d'acceleration de Y comme suit : 
-				Etape 1. : l'accélération dépasse 10 en valeur absolue (on est dans la montée)
-				Etape 2. : L'accélération s'arrete 
-				Etape 3 : L'accélération va dépasser 10 en valeur absolue (on est dans la descente)
-				Etape 4 : l'accélération s'arrete (on a finit notre mouvement)
-			*/
-
-
-
-			var dataY = Math.abs(motion.acceleration.y);
-			var dataX = Math.abs(motion.acceleration.x);
-			//console.warn("Acc : %s",dataY);
-			if (dataY > 10){				
-				timeStep = new Date().getTime();
-				if (lastMax < dataY){
-					lastMax = dataY;
-				}else if (!step1){
-					step1 =  true;
-					socket.emit('message',{
-						action : "jump"
-					});
-					console.info('Jump');
-				}else if (step2 && !step3){
-					step3 = true;
-				}
-			}else if (step1){
-				lastMax = 0;
-				if (!step2){
-					step2 = true;
-				}else if (step3){					
-					//console.info('Reset');
-					step1 = false;
-					step2 = false;
-					step3 = false;					
-				}
-			}
-
-			if (!step1){
-				console.info("Move x : %s ",dataX);
-			}
-
-
-			
-		}, true);
+		window.addEventListener('devicemotion', motionCallBack, true);
+		document.querySelector('.parent').addEventListener('touchstart', touchCallBack, false);
+		document.querySelector('.parent').addEventListener('touchend', touchCallBack, false);
 
 		var validateElemnt = document.querySelector('.validate');
 		validateElemnt.addEventListener('click', function(){
